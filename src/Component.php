aa0f5df13c7b8f07865d81cd2bf97d2995f66c81
Leo <losoviz@gmail.com>
2020-02-27 22:58:40 +0800

    Set NoCache when field/directive involved in cache control is in query

--- src/Component.php
+++ src/Component.php
@@ -11,6 +11,7 @@ use PoP\Root\Component\CanDisableComponentTrait;
  */
 class Component extends AbstractComponent
 {
+    public static $COMPONENT_DIR;
     use YAMLServicesTrait, CanDisableComponentTrait;
     // const VERSION = '0.1.0';
 
@@ -21,12 +22,33 @@ class Component extends AbstractComponent
     {
         if (self::isEnabled()) {
             parent::init();
-            self::initYAMLServices(dirname(__DIR__));
+            self::$COMPONENT_DIR = dirname(__DIR__);
+            self::initYAMLServices(self::$COMPONENT_DIR);
+
+            // Boot conditional on API package being installed
+            if (class_exists('\PoP\CacheControl\Component')) {
+                \PoP\AccessControl\Conditional\CacheControl\ConditionalComponent::init();
+            }
         }
     }
 
     protected static function resolveEnabled()
     {
         return !Environment::disableAccessControl();
+    }
+
+    /**
+     * Boot component
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // Boot conditional on API package being installed
+        if (class_exists('\PoP\CacheControl\Component')) {
+            \PoP\AccessControl\Conditional\CacheControl\ConditionalComponent::boot();
+        }
     }
 }
--- src/TypeResolverDecorators/AbstractPublicSchemaTypeResolverDecorator.php
+++ src/TypeResolverDecorators/AbstractPublicSchemaTypeResolverDecorator.php
@@ -2,11 +2,13 @@
 namespace PoP\AccessControl\TypeResolverDecorators;
 
 use PoP\AccessControl\Environment;
+use PoP\Hooks\Facades\HooksAPIFacade;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 use PoP\ComponentModel\TypeResolverDecorators\AbstractTypeResolverDecorator;
 
 abstract class AbstractPublicSchemaTypeResolverDecorator extends AbstractTypeResolverDecorator
 {
+    public const HOOK_ENABLED = __CLASS__.':enabled';
     /**
      * Enable only for public schema
      *
@@ -15,6 +17,18 @@ abstract class AbstractPublicSchemaTypeResolverDecorator extends AbstractTypeRes
      */
     public function enabled(TypeResolverInterface $typeResolver): bool
     {
-        return !Environment::usePrivateSchemaMode();
+        // By default, this value says: it is only enabled for the public schema.
+        // However, this value can be overriden! If returning true, it will work for both public and private schemas
+        // This is to allow the CacheControl to not cache the response whenever any field/directive involved in Access Control is added to the query
+        // That logic relies on the NoCacheDirective being attached by the ValidateIsUserLoggedInDirective
+        // Hence, ValidateIsUserLoggedInDirective must be always added, also in the private mode!
+        // It will not have any influence though, since it comes after ValidateDirective, which removes the results,
+        // and then ValidateIsUserLoggedInDirective has `needsIDsDataFields` => true, so it won't be executed
+        // But by it being on the directive pipeline alone we can already attach the NoCacheDirective
+        $hooksAPI = HooksAPIFacade::getInstance();
+        return $hooksAPI->applyFilters(
+            self::HOOK_ENABLED,
+            !Environment::usePrivateSchemaMode()
+        );
     }
 }
