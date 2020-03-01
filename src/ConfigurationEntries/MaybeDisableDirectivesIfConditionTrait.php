aa79fa3dde3e1f86aecd1590cae78cef6d7f4afb
Leo <losoviz@gmail.com>
2020-03-02 07:12:03 +0800

    Moved code from UserState

---
+++ src/ConfigurationEntries/MaybeDisableDirectivesIfConditionTrait.php
@@ -0,0 +1,50 @@
+<?php
+namespace PoP\AccessControl\ConfigurationEntries;
+
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+trait MaybeDisableDirectivesIfConditionTrait
+{
+    /**
+     * Configuration entries
+     *
+     * @return array
+     */
+    abstract protected function getEntryList(): array;
+
+    /**
+     * Directive names to remove
+     *
+     * @return array
+     */
+    protected function getDirectiveResolverClasses(): array
+    {
+        return array_map(
+            function($entry) {
+                // The entry has format [directiveResolverClass, value]
+                // So, in position [0], will always be the $directiveResolverClass
+                return $entry[0];
+            },
+           $this->getEntryList()
+        );
+    }
+
+    /**
+     * Filter all the entries from the list which apply to the passed typeResolver and fieldName
+     *
+     * @param boolean $include
+     * @param array $entryList
+     * @param TypeResolverInterface $typeResolver
+     * @param string $fieldName
+     * @return boolean
+     */
+    protected function getMatchingEntriesFromConfiguration(array $entryList, string $state): array
+    {
+        return array_filter(
+            $entryList,
+            function($entry) use($state) {
+                return $entry[1] == $state;
+            }
+        );
+    }
+}
---
+++ src/ConfigurationEntries/MaybeDisableFieldsIfConditionTrait.php
@@ -0,0 +1,51 @@
+<?php
+namespace PoP\AccessControl\ConfigurationEntries;
+
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+trait MaybeDisableFieldsIfConditionTrait
+{
+    /**
+     * Configuration entries
+     *
+     * @return array
+     */
+    abstract protected static function getEntryList(): array;
+
+    /**
+     * Field names to remove
+     *
+     * @return array
+     */
+    protected function getFieldNames(): array
+    {
+        return array_map(
+            function($entry) {
+                // The tuple has format [typeResolverClass, fieldName] or [typeResolverClass, fieldName, $role] or [typeResolverClass, fieldName, $capability]
+                // So, in position [1], will always be the $fieldName
+                return $entry[1];
+            },
+           static::getEntryList()
+        );
+    }
+
+    /**
+     * Filter all the entries from the list which apply to the passed typeResolver and fieldName
+     *
+     * @param boolean $include
+     * @param array $entryList
+     * @param TypeResolverInterface $typeResolver
+     * @param string $fieldName
+     * @return boolean
+     */
+    protected function getMatchingEntriesFromConfiguration(array $entryList, TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $typeResolverClass = get_class($typeResolver);
+        return array_filter(
+            $entryList,
+            function($entry) use($typeResolverClass, $fieldName) {
+                return $entry[0] == $typeResolverClass && $entry[1] == $fieldName;
+            }
+        );
+    }
+}
