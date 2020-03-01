ebb0bafdef8ac03769f0115a692034425efcc21f
Leo <losoviz@gmail.com>
2020-03-01 14:52:05 +0800

    Moved code from UserStateAccessControl

--- src/Hooks/AbstractMaybeDisableDirectivesInPrivateSchemaHookSet.php
+++ src/Hooks/AbstractMaybeDisableDirectivesInPrivateSchemaHookSet.php
@@ -1,18 +1,9 @@
 <?php
 namespace PoP\AccessControl\Hooks;
 
-use PoP\AccessControl\Environment;
 use PoP\AccessControl\Hooks\AbstractMaybeDisableDirectivesHookSet;
 
 abstract class AbstractMaybeDisableDirectivesInPrivateSchemaHookSet extends AbstractMaybeDisableDirectivesHookSet
 {
-    /**
-     * Return true if the directives must be disabled
-     *
-     * @return boolean
-     */
-    protected function enabled(): bool
-    {
-        return Environment::usePrivateSchemaMode();
-    }
+    use MaybeDisableDirectivesInPrivateSchemaHookSetTrait;
 }
