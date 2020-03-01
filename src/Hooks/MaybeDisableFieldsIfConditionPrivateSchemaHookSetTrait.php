f6b3fd67410c263094aa846f400795b9cdf2653d
Leo <losoviz@gmail.com>
2020-03-02 07:21:11 +0800

    Moved code to AccessControl

---
+++ src/Hooks/MaybeDisableFieldsIfConditionPrivateSchemaHookSetTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace PoP\AccessControl\Hooks;
+
+use PoP\AccessControl\ConfigurationEntries\MaybeDisableFieldsIfConditionTrait;
+
+trait MaybeDisableFieldsIfConditionPrivateSchemaHookSetTrait
+{
+    use MaybeDisableFieldsIfConditionTrait;
+
+    protected function enabled(): bool
+    {
+        return parent::enabled() && !empty(static::getEntryList());
+    }
+}
