64629d8365fd5d52c3b2383abfa9a6a16a1e2697
Leo <losoviz@gmail.com>
2020-03-02 11:35:56 +0800

    Removed duplicated code

--- src/TypeResolverDecorators/ValidateConditionForDirectivesTypeResolverDecoratorTrait.php
+++ src/TypeResolverDecorators/ValidateConditionForDirectivesTypeResolverDecoratorTrait.php
@@ -1,18 +1,12 @@
 <?php
 namespace PoP\AccessControl\TypeResolverDecorators;
 
-use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 use PoP\AccessControl\ConfigurationEntries\MaybeDisableDirectivesIfConditionTrait;
 use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 
 trait ValidateConditionForDirectivesTypeResolverDecoratorTrait
 {
     use MaybeDisableDirectivesIfConditionTrait;
-
-    public function enabled(TypeResolverInterface $typeResolver): bool
-    {
-        return parent::enabled($typeResolver) && !empty($this->getEntryList());
-    }
 
     /**
      * Because the validation can be done on any directive applied to any typeResolver, then attach it to the base abstract class: AbstractTypeResolver::class
