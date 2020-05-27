b196e3e52c3e58c9e63326da375fd8fbc5ba6c8b
Leonardo Losoviz <leo@getpop.org>
2020-05-27 11:10:56 +0800

    Set configuration thru ComponentConfigurationTrait

--- src/Component.php
+++ src/Component.php
@@ -30,10 +30,10 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      */
-    protected static function doInitialize(bool $skipSchema = false): void
+    protected static function doInitialize(array $configuration = [], bool $skipSchema = false): void
     {
         if (self::isEnabled()) {
-            parent::doInitialize($skipSchema);
+            parent::doInitialize($configuration, $skipSchema);
             self::$COMPONENT_DIR = dirname(__DIR__);
             self::initYAMLServices(self::$COMPONENT_DIR);
             self::maybeInitYAMLSchemaServices(self::$COMPONENT_DIR, $skipSchema);
