ffdbb01942428b5a92c9ba5f13a696075479229e
Leonardo Losoviz <leo@getpop.org>
2020-10-14 13:21:08 +0800

    Use autowiring

--- src/Facades/AccessControlManagerFacade.php
+++ src/Facades/AccessControlManagerFacade.php
@@ -14,7 +14,7 @@ class AccessControlManagerFacade
         /**
          * @var AccessControlManagerInterface
          */
-        $service = ContainerBuilderFactory::getInstance()->get('access_control_manager');
+        $service = ContainerBuilderFactory::getInstance()->get(AccessControlManagerInterface::class);
         return $service;
     }
 }
